493d123ec55886fbe582a72a10056e7c3f090a0d
serkan <ozgr.serkan09@gmail.com>
2022-02-03 12:52:30 +0300

    app.js edited for slider

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const wrapper = document.querySelector(".sliderWrapper");
-console.log(wrapper);
+const menuItems = document.querySelectorAll(".menuItem");
 
-wrapper.style.backgroundColor = "red";
+menuItems.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    wrapper.style.transform = `translateX(${-100 * index}vw)`;
+  });
+});
